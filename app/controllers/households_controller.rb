be6574a3698db130073e84c14c9d4fd3d4c5960f
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-02-14 10:15:32 -0800

    adding check boxes

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -6,11 +6,20 @@ class HouseholdsController < ApplicationController
     end
 
     post '/households' do
+        #params => {"user"=>{"household_ids"=>["1", "5", "6", "7", "8", "9"]}, "household"=>{"name"=>""}}
+        if !params["user"]["household_ids"].empty?
+            current_user.household_ids = params["user"]["household_ids"]
+        end
         if !params["household"]["name"].empty?
+            binding.pry
             @household = Household.create(name: params["household"]["name"], owner_id: current_user.id)
+            current_user.household_ids << @household.id
+            current_user.save
+            # user_household = UserHousehold.create(user_id: current_user.id, household_id: @household.id)
         end
 
-        redirect to "/households/#{@household[:id]}"
+        redirect to '/home'
+
     end
 
     get '/households/:id' do 
@@ -27,6 +36,12 @@ class HouseholdsController < ApplicationController
         household = Household.find(params[:id])
         household.update(params["household"])  
         redirect to '/home'
+    end
+
+    delete '/households/:id/delete' do
+        @household = Household.find(params[:id])
+        @household.delete
+        redirect to '/home' 
     end
 
 end
