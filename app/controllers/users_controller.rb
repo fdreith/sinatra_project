985e9fb51203fef97cd18b77451f4bb9397003c0
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-02-13 20:07:30 -0800

    Added user's controller and separating concerns

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+    get '/signup' do
+        @households = Household.all
+        erb :'users/signup'
+    end
+    
+    post '/signup' do
+        user = User.new(username: params["user"]["username"], password: params["user"]["password"])
+        if user.save
+            session[:user_id]=user.id
+            redirect to '/home'
+        else
+            erb :error
+            redirect to '/signup'
+        end
+    end
+
+    get '/home' do 
+        if logged_in? 
+            user = User.find(current_user.id)
+            @households = user.households
+            erb :'users/home'
+        else
+            erb :error
+        end
+    end
+end
