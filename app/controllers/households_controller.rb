5ef3183ff5b5aa99d809728f86108e354ff264ee
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-02-13 20:11:39 -0800

    Added household show by id and update

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -1,33 +1,45 @@
 class HouseholdsController < ApplicationController
 
     get '/households/new' do
-        @pets = Pet.all
+        @households = Household.all
         erb :'households/new'
     end
 
     post '/households' do
-        household = Household.create(name: params[:name], owner_id: current_user[:id])
-        # if household.save
         if !params["household"]["name"].empty?
-            @user.households << Household.create(name: params["household"]["name"], owner_id: current_user[:id])
+            household = Household.create(name: params["household"]["name"], owner_id: current_user.id)
         end
-            
-    
+        
+        redirect to "/households/#{params[:id]}"
+    end
 
+    get '/households/:id' do 
+        @household = Household.find(params[:id])
+        erb :'/households/show'
     end
 
     get '/households/:id/edit' do 
         @household = Household.find(params[:id])
         erb :'/households/edit'
     end
-    
-    get '/households/:id' do 
-        @household = Household.find(params[:id])
-        erb :'/households/show'
-    end
 
     patch '/households/:id' do 
-    
+        household = Household.find(params[:id])
+        household.update(params["household"])   #pets as a stretch goal)
+        redirect to '/home'
+
+    #     #stretch updating pets:
+    #     # if !params[:household].keys.include?("pet_ids")
+    #     #     params[:household]["pet_ids"] = []
+    #     #   end
+      
+    #     #   @household = Household.find(params[:id])
+    #     #   @household.update(params["household"])
+    #     #   if !params["pet"]["name"].empty?
+    #     #     @household.pets << Pet.create(name: params["pet"]["name"])
+    #     #   end
     end
+    
+
     
 end
