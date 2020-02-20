dbd27ad94bcbda23f50f0e545d371003ffb30b92
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-02-19 16:01:48 -0800

    getting rid of unnecessary syntax

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
     end
     
     post '/login' do
-        user = User.find_by(username: params["user"][":username"].strip)
-        if user && user.authenticate(params["user"][":password"])
+        user = User.find_by(username: params["user"]["username"].strip)
+        if user && user.authenticate(params["user"]["password"])
             session[:user_id]=user.id
             redirect to "/home"
         else
