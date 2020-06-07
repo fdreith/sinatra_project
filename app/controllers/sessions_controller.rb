04d5e53cc16bcf8bfb9ab52273e0c4b23241a1e3
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-06-07 11:53:46 -0700

    fixing login error and signup formating

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     end
     
     post '/login' do
+        binding.pry
         user = User.find_by(username: params["user"]["username"].strip)
         if user && user.authenticate(params["user"]["password"])
             session[:user_id]=user.id
